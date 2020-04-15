3975833ba1c237317ef35e598ccdc6426adb5f65
JainalArifin <zainalunpam05@gmail.com>
2020-04-15 17:16:43 +0700

    initial commit

---
+++ app/Http/Controllers/Api/QuoteController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\QuoteResource;
+use App\Quote;
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller
+{
+    public function index()
+    {
+        $quote =  Quote::latest()->paginate(5);
+
+        return QuoteResource::collection($quote);
+    }
+
+    public function show($id)
+    {
+        $quote = Quote::find($id);
+        if(is_null($quote))
+        {   
+            return response()->json([
+                "error" => "Resource not found"
+            ], 404);
+        }
+        return new QuoteResource($quote);
+    }
+
+    public function update(Request $request, Quote $quote)
+    {
+        $this->authorize('update', $quote);
+        $quote->update([
+            'message' => $request->message
+        ]);
+        
+        return new QuoteResource($quote);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'message' => 'required'
+        ]);
+
+        $quote = Quote::create([
+            'user_id' => auth()->id(),
+            'message' => $request->message
+        ]);
+
+        return new QuoteResource($quote);
+    }
+
+    public function destroy(Quote $quote)
+    {
+        $this->authorize('delete', $quote);
+        $quote->delete();
+
+        return response()->json([
+            'message' => 'Quote deleted'
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/register', 'Api\RegisterController@action');
+Route::post('/login', 'Api\LoginController@action');
+Route::get('/me', 'Api\UserController@me')->middleware('auth:api');
+Route::post('/quote', 'Api\QuoteController@store')->middleware('auth:api');
+Route::get('/quote', 'Api\QuoteController@index')->middleware('auth:api');
+Route::get('/quote/{id}', 'Api\QuoteController@show')->middleware('auth:api');
+Route::put('/quote/{quote}', 'Api\QuoteController@update')->middleware('auth:api');
+Route::delete('/quote/{quote}', 'Api\QuoteController@destroy')->middleware('auth:api');
